9364fc97bbc79bcb59100e00175a5ca30f56aaf0
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-11-12 17:00:17 -0600

    Update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@
 # )
 
 ''' https://colab.research.google.com/github/i-am-shuan/learn-langchain/blob/main/langchain_RAG_example.ipynb#scrollTo=S79kQe04X2-j '''
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFLoader # type: ignore
 
 # loader = PyPDFLoader("https://snuac.snu.ac.kr/2015_snuac/wp-content/uploads/2015/07/asiabrief_3-26.pdf")
 loader = PyPDFLoader("./asiabrief_3-26.pdf")
@@ -19,7 +19,7 @@ print(pages)
 print('\n\n')
 
 # PDF 내용을 작은 chunk 단위로 나누기
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter # type: ignore
 ''' split characters  ["\n\n", "\n", " ", ""]'''
 ''' length_function , chunk_size , chunk_overlap, add_start_index (Determines whether to include the start position of the chunk within the original document in the metadata) '''
 
